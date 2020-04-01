e19b0b880328763d736f4ce3285fdf25945e9398
lilianyangc <mlilianyang@gmail.com>
2020-04-01 16:09:21 -0400

    Tabs added

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Admin.css';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import history from "../History";
+import {Container} from 'react-bootstrap';
+import ChatHistory from '../ChatHistory';
+import EventHistory from '../EventHistory';
+import Rooms from '../Rooms';
+import { RoutedTabs, NavTab } from "react-router-tabs";
+
 
 class Admin extends Component {
   constructor(props){
@@ -24,6 +32,39 @@ class Admin extends Component {
       <>
         <div>Admin Home</div>
         <div><button onClick={this.logout}>Logout</button></div>
+        <Container>
+        <BrowserRouter>
+        {/* <Nav variant="tabs" defaultActiveKey="/admin">
+          <Nav.Item>
+            <Nav.Link eventKey="link-0"><Link to="/admin">Event History</Link></Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1"><Link to="/chathistory">Chat History</Link></Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link to="/rooms" eventKey="link-2"><Link to="/rooms">Rooms</Link></Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <ul>
+          <li><Link to="/admin">Home</Link></li>
+          <li><Link to="/chathistory">About</Link></li>
+          <li><Link to="/rooms">Contact</Link></li>
+        </ul> */}
+        <NavTab to="/admin">Event History</NavTab> &nbsp;
+        <NavTab to="/chathistory">Chat History</NavTab>&nbsp;
+        <NavTab to="/rooms">Rooms</NavTab>
+        <br />
+
+        <Container>
+            <Switch>
+              <Route path="/admin" component={EventHistory} exact/>
+              <Route path="/chathistory" component={ChatHistory} />
+              <Route path="/rooms" component={Rooms} />
+            </Switch>
+        </Container>       
+        </BrowserRouter>
+     
+      </Container>
       </>
     );
   }
