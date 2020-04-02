e8dd5ed04c5ae32095381aae5c0999973499a458
Francis Victa <francisvicta45@gmail.com>
2020-04-02 18:11:41 -0400

    added Add Room functionality

---
+++ src/components/AddRoom.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import {Modal,Form} from "react-bootstrap";
+import axios from "axios";
+import Button from '@material-ui/core/Button';
+
+function RoomAdd() {
+  const [show, setShow] = useState(false);
+  const [roomName, setRoomName] = useState('');
+  const [roomStatus, setRoomStatus] = useState('Active');
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChangeRoom = (event) => {
+    setRoomName(event.target.value);
+  }
+  const handleChangeStatus = (event) => {
+      setRoomStatus(event.target.value);
+  }
+  const addRoom = () => {
+    //Get Current Date and Time
+    var date = Date(Date.now());
+    var dateStringify = date.toString();
+
+    //send post request to create room
+    axios
+      .post(
+        "http://chat-masters.herokuapp.com/api/new-room",
+        {   
+            name: roomName,
+            lastEdit: dateStringify,
+            status: roomStatus 
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      )
+      .then(res => {
+        console.log(res);
+        alert('A Room was created!');
+        handleClose();
+        window.location.reload();
+      });
+  }
+ 
+  return (
+    <>
+      <div style={{ width: "10em", margin: "1em", marginTop: "2em" }}>
+        <Button variant="contained" color="primary" onClick={handleShow}>Add Room</Button>
+      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Room</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={addRoom}>
+            <Form.Group controlId="roomName">
+              <Form.Label>Room Name</Form.Label>
+              <Form.Control onChange={handleChangeRoom} type="text" required />
+            </Form.Group>
+            <Form.Group controlId="roomStatus">
+              <Form.Label>Status</Form.Label>
+              <Form.Control  as="select" value={roomStatus} onChange={handleChangeStatus}>
+                <option>Active</option>
+                <option>Inactive</option>
+              </Form.Control>
+            </Form.Group>
+          <Modal.Footer>
+                <Button variant="contained" onClick={handleClose}>Cancel</Button>
+                <Button variant="contained" color="primary" type="submit">Save</Button>
+          </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default RoomAdd;
