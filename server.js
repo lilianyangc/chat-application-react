34b35e5abcbb913d1de82a1d73932e6b947c511d
Francis Victa <francisvicta45@gmail.com>
2020-04-09 18:46:31 -0400

    edited server.js

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'build')))
 
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 8080
 app.listen(port)
 console.log(`server listening on port: ${port}`)
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
 });
